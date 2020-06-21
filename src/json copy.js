232930dbee3d4adb0207d7c011e0b23ba8e21f6c
Subhash Jha <subhashjha.mail@gmail.com>
2020-06-21 15:06:46 +0530

    adding data

---
+++ src/json copy.js
@@ -0,0 +1,565 @@
+export default {
+	name: 'Nathu Jha',
+	children:[{
+		name: 'Narhari Jha',
+		children:[{
+			name: 'Vasudev Jha',
+			children:[{
+				name: 'Indrapati Jha',
+				children:[{
+					name: 'Keshav Jha',
+					children:[{
+						name: 'Shyam Jha',
+						children:[{
+							name: 'Anirudh Jha',
+							children:[{
+								name: 'Udayram Jha',
+								children:[{
+									name: 'Khedan Jha',
+									children:[{
+										name: 'Dukhran Jha',
+										children:[{
+											name: 'Baburam Jha',
+											children:[{
+												name:'Kashinath Jha',
+												children:[{
+													name:'Janaklal Jha',
+													children:[{
+														name:'Umakant Jha',
+														children:[{
+															name:'Babusaheb Jha',
+															children:[{
+																name:'Bharat Jha',
+																children:[]
+															},{
+																name:'Shatrughan Jha',
+																children:[]
+															}]
+														},{
+															name:'Pitambar Jha',
+															children:[{
+																name:'Rakesh Jha',
+																children:[]
+															}]
+														}]
+													}]
+												},{
+													name:'Madhushudan Jha',
+													children:[{
+														name:'Bageshwar Jha',
+														children:[{
+															name:'Lalbuchchi Devi',
+															children:[]
+														}]
+													}]
+												}]
+											},{
+												name:'Vishwanath Jha',
+												children:[]
+											},{
+												name:'Sashinath Jha',
+												children:[{
+													name:'Jayanath Jha',
+													children:[]
+												},{
+													name:'Janardhan Jha',
+													children:[{
+														name:'Jayaveesh Jha',
+														children:[]
+													}]
+												}]
+											}]
+										},{
+											name: 'Dheernath Jha',
+											children:[{
+												name: 'Raghuvar Jha',
+												children:[]
+											},{
+												name: 'Gopinath Jha',
+												children:[]
+											},{
+												name: 'Nenmani Jha',
+												children:[{
+													name: 'Ugramani Jha',
+													children:[]
+												},{
+													name: 'Krishnamani Jha',
+													children:[{
+														name: 'Suvansh Lal Jha',
+														children:[{
+															name: 'Madhukant Jha',
+															children:[{
+																name: 'Kulanand Jha',
+																children:[]
+															},{
+																name: 'Raghunath Jha',
+																children:[]
+															},{
+																name: 'Krityanand Jha',
+																children:[]
+															}]
+														},{
+															name: 'Shubhkant Jha',
+															children:[{
+																name: 'Ashok Jha',
+																children:[]
+															},{
+																name: 'Kishore Jha',
+																children:[]
+															},{
+																name: 'Amol Jha',
+																children:[]
+															}]
+														},{
+															name: 'Mahakant Jha',
+															children:[{
+																name: 'Chandrashekhar Jha',
+																children:[{
+																	name: 'Child 1',
+																	children:[]
+																},{
+																	name: 'Child 2',
+																	children:[]
+																}]
+															},{
+																name: 'Ajeet Jha',
+																children:[]
+															}]
+														}]
+													},{
+														name: 'Yaduvansh Jha',
+														children:[{
+															name: 'Lakshmikant Jha',
+															children:[{
+																name: 'Sadanand Jha',
+																children:[]
+															}]
+														},{
+															name: 'Dayakant Jha',
+															children:[{
+																name: 'Tulanand Jha',
+																children:[{
+																	name: 'Chandan Jha',
+																	children:[]
+																}]
+															},{
+																name: 'Lokanand Jha',
+																children:[]
+															},{
+																name: 'Parmanand Jha',
+																children:[]
+															}]
+														}]
+													},{
+														name: 'Srivansh Jha',
+														children:[{
+															name: 'Jeevach Jha',
+															children:[{
+																name: 'Sushil Kumar Jha',
+																children:[]
+															}]
+														},{
+															name: 'ShobhaNand Jha',
+															children:[{
+																name: 'Sanjeev Jha',
+																children:[]
+															},{
+																name: 'Rajeev Jha',
+																children:[]
+															}]
+														}]
+													},{
+														name: 'Udayvansh Jha',
+														children:[{
+															name: 'Anmeghana Devi',
+															children:[]
+														}]
+													},{
+														name: 'Lakshman Jha',
+														children:[{
+															name: 'Shyamakant Jha',
+															children:[{
+																name: 'Vaidyanath Jha',
+																children:[{
+																	name: 'Roushan Jha',
+																	children:[]
+																}]
+															},{
+																name: 'Vishwanath Jha',
+																children:[{
+																	name: 'Vipin Vatsa',
+																	children:[]
+																},{
+																	name: 'Nitin Vatsa',
+																	children:[]
+																}]
+															}]
+														}]
+													}]
+												},{
+													name: 'Jayadev Jha',
+													children:[{
+														name: 'Mahadev Jha',
+														children:[{
+															name: 'Mohan Jha',
+															children:[{
+																name: 'Pankaj Jha',
+																children:[]
+															}]
+														},{
+															name: 'Lal Jha',
+															children:[]
+														},{
+															name: 'Rajendra Jha',
+															children:[]
+														}]
+													}]
+												},{
+													name: 'Ramdev Jha',
+													children:[{
+														name: 'Dharmeshwar Jha',
+														children:[{
+															name: 'Sanjay Kumar Jha',
+															children:[]
+														}]
+													},{
+														name: 'Umeshwar Jha',
+														children:[{
+															name: 'Vinod Jha',
+															children:[]
+														},{
+															name: 'Manoj Jha',
+															children:[{
+																name: 'Raja Babu',
+																children:[]
+															},{
+																name: 'Child 2',
+																children:[]
+															}]
+														},{
+															name: 'Shailendra Jha',
+															children:[]
+														},{
+															name: 'Amrendra Jha',
+															children:[]
+														}]
+													},{
+														name: 'Dineshwar Jha',
+														children:[]
+													},{
+														name: 'Maheshwar Jha',
+														partner: {
+															name: 'Ambika Jha',
+														},
+														children: [{
+															name: 'Manju Jha',
+															partner: {
+																name: 'Hemdhar Jha',
+															},
+															children: [{
+																name: 'Ambikesh Jha',
+																partner: {
+																	name: 'Anu Jha',
+																},
+																children: [{
+																	name: 'Aayushi Jha',
+																	children: []
+																},{
+																	name: 'Adya Jha',
+																	children: []
+																}]
+															}, {
+																name: 'Rajesh Jha',
+																partner: {
+																	name: 'Nirjala Jha',
+																},
+																children: [{
+																	name: 'Rajat Jha',
+																	children: []
+																},{
+																	name: 'Aditya Jha',
+																	children: []
+																}]
+															}, {
+																name: 'Kamlesh Jha',
+																partner: {
+																	name: 'Vandana Jha',
+																},
+																children: [{
+																	name: 'Harshit Jha',
+																	children: []
+																},{
+																	name: 'Kartik Jha',
+																	children: []
+																}]
+															}, {
+																name: 'Shailesh Jha',
+																partner: {
+																	name: 'Aruna Jha',
+																},
+																children: [{
+																	name: 'Yash',
+																	children: []
+																},{
+																	name: 'Juhi',
+																	children: []
+																}]
+															}]
+														}, {
+															name: 'Arvind Jha',
+															partner: {
+																name: 'Sabri Devi',
+															},
+															children: [{
+																name: 'Jaya Jha',
+																partner: {
+																	name: 'Awadhesh Jha',
+																},
+																children: [{
+																	name: 'Aryaman Jha',
+																	children: []
+																}]
+															}, {
+																name: 'Jyoti Pathak',
+																partner: {
+																	name: 'Rajesh Pathak',
+																},
+																children: [{
+																	name: 'Rashi Pathak',
+																	children: []
+																},{
+																	name: 'Aastha Pathak',
+																	children: []
+																}]
+															}, {
+																name: 'Prakash Jha',
+																partner: {
+																	name: 'Rishu Shandilya',
+																},
+																children: []
+															}, {
+																name: 'Preeti Jha',
+																partner: {
+																	name: 'Swapnil Pathade',
+																},
+																children: []
+															}]
+														}, {
+															name: 'Sunita Jha',
+															partner: {
+																name: 'Bihari Jha',
+															},
+															children: [{
+																name: 'Chandan Jha',
+																partner: {
+																	name: 'Kabita Jha',
+																},
+																children: [{
+																	name: 'Vinamra Jha',
+																	children: []
+																}]
+															}, {
+																name: 'Rachna Jha',
+																partner: {
+																	name: 'Santosh Jha',
+																},
+																children: [{
+																	name: 'Karnika Jha',
+																	children: []
+																},{
+																	name: 'Umang Jha',
+																	children: []
+																}]
+															}]
+														}, {
+															name: 'Birendra Jha',
+															partner: {
+																name: 'Renu Jha',
+															},
+															children: [{
+																name: 'Vikas Jha',
+																partner: {
+																	name: 'Niharica Jha',
+																},
+																children: []
+															}, {
+																name: 'Subhash Jha',
+																partner: {
+																	name: 'Renuka Mishra',
+																},
+																children: []
+															}, {
+																name: 'Shilpa Bharati',
+																children: []
+															}]
+														}, {
+															name: 'Dhirendra Jha',
+															partner: {
+																name: 'Usha Jha',
+															},
+															children: [{
+																name: 'Neha Jha',
+																children: []
+															}, {
+																name: 'Nupur Jha',
+																children: []
+															}]
+														}, {
+															name: 'Sujata Jha',
+															partner: {
+																name: 'Amar Nath Jha',
+															},
+															children: [{
+																name: 'Kadambari Jha',
+																partner: {
+																	name: 'BK Jha',
+																},
+																children: [{
+																	name: 'Child 1',
+																	children: []
+																},{
+																	name: 'Child 2',
+																	children: []
+																}]
+															}, {
+																name: 'Shyam Nath Jha',
+																partner: {
+																	name: 'Anjana Jha',
+																},
+																children: [{
+																	name: 'Atharva Jha',
+																	children: []
+																}]
+															}, {
+																name: 'Priya Jha',
+																children: [{
+																	name: 'Rishi Jha',
+																	children: []
+																}]
+															}, {
+																name: 'Aditya Nath Jha',
+																partner: {
+																	name: 'Priyanka Jha',
+																},
+																children: [{
+																	name: 'Child 1',
+																	children: []
+																}, {
+																	name: 'Child 2',
+																	children: []
+																}]
+															}]
+														}]
+													},{
+														name: 'Ugeshwar Jha',
+														children:[]
+													}]
+												},{
+													name: 'Baldev Jha',
+													children:[{
+														name: 'Sri Nandan Jha',
+														children:[{
+															name: 'Surya Kant Jha',
+															children:[{
+																name: 'Santosh Jha',
+																children:[]
+															},{
+																name: 'Mantosh Jha',
+																children:[]
+															},{
+																name: 'Ranjit Jha',
+																children:[]
+															},{
+																name: 'Sunil Jha',
+																children:[]
+															}]
+														}]
+													},{
+														name: 'Dev Nandan Jha',
+														children:[{
+															name: 'Harikant Jha',
+															children:[{
+																name: 'Lalan Jha',
+																children:[{
+																	name: 'Goopal Jha',
+																	children:[]
+																},{
+																	name: 'Child 2',
+																	children:[]
+																},{
+																	name: 'Child 3',
+																	children:[]
+																}]
+															},{
+																name: 'Madan Jha',
+																children:[]
+															},{
+																name: 'Pawan Jha',
+																children:[]
+															}]
+														},{
+															name: 'Gangakant Jha',
+															children:[{
+																name: 'Pitamber Jha',
+																children:[]
+															},{
+																name: 'Bhavan Jha',
+																children:[]
+															},{
+																name: 'Krishnakant Jha',
+																children:[]
+															}]
+														},{
+															name: 'Mayakant Jha',
+															children:[{
+																name: 'Vishwambhar Jha',
+																children:[{
+																	name: 'Child 1',
+																	children:[]
+																},{
+																	name: 'Child2',
+																	children:[]
+																}]
+															},{
+																name: 'Raktambar Jha',
+																children:[{
+																	name: 'Child 1',
+																	children:[]
+																},{
+																	name: 'Child2',
+																	children:[]
+																}]
+															},{
+																name: 'Pappu Jha',
+																children:[]
+															}]
+														}]
+													}]
+												}]
+											},{
+												name: 'Kulmani Jha',
+												children:[]
+											},{
+												name: 'Rudramani Jha',
+												children:[]
+											}]
+										},{
+											name: 'Dullah Jha',
+											children:[{
+												name:'Kaladhar Jha',
+												children:[]
+											},{
+												name:'Leeladhar Jha',
+												children:[]
+											}]
+										}]
+									}]
+								}]
+							}]
+						}]
+					}]
+				}]
+			}]
+		}]
+	}]
+};
