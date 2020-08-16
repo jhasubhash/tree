8cfa5bb1748ad1b1fe4c7c48a67bc580b282ef15
Subhash Jha <subhashjha.mail@gmail.com>
2020-08-16 17:08:40 +0530

    Updates

--- precache-manifest.bb2b73fb75663fc19e4c0d2521814013.js
+++ precache-manifest.bb2b73fb75663fc19e4c0d2521814013.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d162f40802323a56ac18185299b40d0c",
+    "revision": "ad8d58f3775daa558f91d83fda4c6dc3",
     "url": "/tree/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tree/static/css/2.fbd218b0.chunk.css"
   },
   {
-    "revision": "fdd0cf0efab7fda961e9",
-    "url": "/tree/static/css/main.1c0a17e8.chunk.css"
+    "revision": "af1278c5501950d0cb81",
+    "url": "/tree/static/css/main.638fe4e3.chunk.css"
   },
   {
     "revision": "431b40fa0d333ecab915",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tree/static/js/2.255071a9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fdd0cf0efab7fda961e9",
-    "url": "/tree/static/js/main.f72cfa6b.chunk.js"
+    "revision": "af1278c5501950d0cb81",
+    "url": "/tree/static/js/main.0b9ea184.chunk.js"
   },
   {
     "revision": "eb80b0e7e3ddc5cf0950",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tree/precache-manifest.604151abbfd5da1fe3d7c2db62ed83b1.js"
+  "/tree/precache-manifest.bb2b73fb75663fc19e4c0d2521814013.js"
 );
 
 self.addEventListener('message', (event) => {
