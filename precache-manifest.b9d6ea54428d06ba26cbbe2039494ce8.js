404ae2e8bc64076629fa903969818358a5a5d422
Subhash Jha <subhashjha.mail@gmail.com>
2021-02-17 23:25:00 +0530

    Revert "Updates"
    This reverts commit 513d67a163123a4601626b926dc93bac5c95ae4b.

---
+++ precache-manifest.b9d6ea54428d06ba26cbbe2039494ce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff5ea3a4d8e88b8893e038381776d04",
+    "url": "/tree/index.html"
+  },
+  {
+    "revision": "9996ef3aec90868e3253",
+    "url": "/tree/static/css/2.fbd218b0.chunk.css"
+  },
+  {
+    "revision": "721a70f23a7cb86f3fd6",
+    "url": "/tree/static/css/main.9d7d402a.chunk.css"
+  },
+  {
+    "revision": "9996ef3aec90868e3253",
+    "url": "/tree/static/js/2.4fa7d8aa.chunk.js"
+  },
+  {
+    "revision": "daaee418f48f1ca2e19bb903b714e262",
+    "url": "/tree/static/js/2.4fa7d8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721a70f23a7cb86f3fd6",
+    "url": "/tree/static/js/main.adfbf8d3.chunk.js"
+  },
+  {
+    "revision": "eb80b0e7e3ddc5cf0950",
+    "url": "/tree/static/js/runtime-main.c89658c5.js"
+  }
+]);
